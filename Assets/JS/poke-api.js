5d8deb35e43a3bdf02a87c7fdf8d6bde3a99e377
denailly <danilocalheirosjr@gmail.com>
2023-01-06 00:44:36 -0300

    adicionado filtro de request para uso de apenas informacoes importantes serem solicitadas a API

--- Assets/JS/poke-api.js
+++ Assets/JS/poke-api.js
@@ -1,7 +1,24 @@
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemons = new Pokemon();
+
+  pokemons.number = pokeDetail.order;
+  pokemons.name = pokeDetail.name;
+  pokemons.picture = pokeDetail.sprites.other.dream_world.front_default;
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemons.types = types;
+  pokemons.type = type;
+
+  return pokemons;
+}
+
 pokeApi.getPokemonDetails = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
 };
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
